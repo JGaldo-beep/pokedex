9f7ec73b80e2416e0fa1d21770536aad8a663d68
JGaldp-beep <juan.galdo@ucb.edu.bo>
2025-03-31 22:14:00 -0400

    Initial commit

---
+++ app/src/main/java/com/example/pokedex/model/Pokemon.kt
@@ -0,0 +1,12 @@
+package com.example.pokedex.model
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Pokemon (
+    @StringRes val name: Int,
+    @StringRes val description: Int,
+    @DrawableRes val image: Int,
+)
+
+// Pokemon("Name", "Description", R.drawable.image)
